b8dc81ca4b7d26d57770d815f4d7b20da489b786
Ihor Neizvestnyi <neizvestnyi.igor@gmail.com>
2022-12-06 21:05:33 +0200

    03

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const http = require('http');
 const url = require('url');
 const { parse } = require('querystring');
 const bodyParser = require('body-parser')
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const client = new MongoClient(process.env.MONGO_URL);
 
 const start = async (req, res) => { 
 	try {
 		await client.connect(); 
+		const trains = client.db().collection('trains');
 		console.log('Connected to DB');
 		console.log(req.method);
 
@@ -22,8 +23,7 @@ const start = async (req, res) => {
 		// 	res.writeHead(200);
     //   res.end("Odd");
 		// 	};
-			
-			const trains = client.db().collection('trains');
+
 			const result = await trains.find().toArray();
 			// console.log(result);
 			// console.log(JSON.stringify(result));
@@ -47,10 +47,37 @@ const start = async (req, res) => {
 				const bodyJson = JSON.parse(body);
 				//console.log(bodyJson);
 
-				const trains = client.db().collection('trains');
 				await trains.insertOne(bodyJson);
 				res.writeHead(200);
 				res.end('Record created!');
+			});
+		};
+
+		if (req.method === 'PUT') {
+			let body = '';
+			req.on('data', chunk => {
+				body += chunk.toString();
+			})
+
+			req.on('end', async () => {
+				const bodyJson = JSON.parse(body);
+				console.log(bodyJson);
+				let urlRequest = url.parse(req.url, true);
+				console.log(urlRequest.query.id);
+				const conditions = {
+					_id: new ObjectId(urlRequest.query.id)
+				};
+
+				await trains.updateOne(conditions, { $set: bodyJson });
+
+				// let train = await trains.findOne({ _id: urlRequest.query.id });
+				// console.log(train);
+
+				const result = await trains.find().toArray();
+
+				res.setHeader('Content-Type', 'application/json');
+				res.writeHead(200);
+				res.end(JSON.stringify(result));
 			});
 		};
 
