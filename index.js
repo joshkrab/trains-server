52b5bfdaa171109d4d3ec9dc1d6213d09ed7f49a
Ihor Neizvestnyi <neizvestnyi.igor@gmail.com>
2022-12-08 23:49:48 +0200

    commit25

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const start = async (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested, X-Requested-With, Content-Type',);	
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader(('Access-Control-Allow-Credentials', true));
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader("Content-Type", "application/json");
 
 	try {
