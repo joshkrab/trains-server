618154a3dce5bbe05cfcaa008da38fa4beb79d7e
Ihor Neizvestnyi <neizvestnyi.igor@gmail.com>
2022-12-08 23:21:21 +0200

    commit19

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client = new MongoClient(process.env.MONGO_URL);
 
 const start = async (req, res) => { 
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept Authorization',);	
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type',);	
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, DELETE, PUT');
   res.setHeader("Content-Type", "application/json");
 
