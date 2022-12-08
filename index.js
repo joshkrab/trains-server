50ef34954e6987f725e874302570d118e552cba9
Ihor Neizvestnyi <neizvestnyi.igor@gmail.com>
2022-12-08 22:06:05 +0200

    commit08

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const start = async (req, res) => {
 		console.log(req.method);
 
 		const headers = {
-			'Content-Type': 'application/json',
 			'Access-Control-Allow-Origin': 'http://localhost:3000',
 			'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, DELETE, PUT',
 			'Access-Control-Max-Age': 2592000,
